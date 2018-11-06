878d76352761583b30494a1879ab4d2d7ac4b386
Petr Přibyl <cutlerry@gmail.com>
2018-11-06 23:01:02 +0100

    test

---
+++ src/core/basic.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ *
+ * @author Petr Pribyl <petr.pribyl@bohemiasoft.com>
+ * @copyright (c) 2018, Bohemiasoft s.r.o. 06.11.2018
+ *
+ */
+
+namespace Core;
+
+
+class Basic
+{
+    public static function Test()
+    {
+        return 'Test';
+    }
+}
